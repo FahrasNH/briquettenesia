0f98b427990b0523e4da699f029aa2051a4bbc6d
imamnghr <imamn208@gmail.com>
2021-11-21 08:38:29 +0700

    about section

--- src/components/LandingPage/AboutSection/index.jsx
+++ src/components/LandingPage/AboutSection/index.jsx
@@ -1,5 +1,19 @@
+import { Row } from 'antd'
+
 const AboutSection = () => {
-    return <h1>About</h1>
+  return (
+    <div className="section-wrap">
+      <h1 className="section-title">About Us</h1>
+      <div className="content">
+        <p>
+        Briquettenesia is a leading manufacturer and exporter of 100% coconut shell charcoal briquettes, from Indonesia. We control every step of the manufacturing and quality control process of our briquettes, from coconut shell selection, cleaning and carbonization all the way through to final briquetting, drying and packaging. Our production locations are situated in all the major coconut harvesting regions of Indonesia in Sumatra, Java and Sulawesi. We supply coconut shell charcoal briquettes in bulk and can ship to any destination worldwide. We can produce briquettes in any desired shape or size and tailor make packaging according to our customers exact design preferences.</p>
+        <p>
+          
+        We insist on producing our own coconut shell charcoal for the majority of our briquette production and also working with a few select suppliers of high grade charcoal, to ensure that the fixed carbon content is high and ash content low. Our briquettes are perfect for use in Shisha/hookah or for barbeque, providing very high heat, long burn time and minimal ash. Due to the high quality charcoal used, our briquettes produce no smoke, odours or sparks while being burned.
+        </p>
+      </div>
+      </div>
+  )
 }
 
 export default AboutSection
