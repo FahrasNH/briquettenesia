f5af66973931bae26f424be2febe7bb34a5c55c5
FahrasNH <fahras1998@yahoo.com>
2021-11-26 23:35:25 +0700

    Create slider header

--- src/components/LandingPage/ProductSection/index.jsx
+++ src/components/LandingPage/ProductSection/index.jsx
@@ -1,80 +1,51 @@
-import { Row, Col } from "antd";
-import { useKeenSlider } from "keen-slider/react";
-import React from "react";
+import React from 'react'
+import { Row, Col } from 'antd'
+import Slider from '../../../modules/Slider'
 
 const ProductSection = () => {
-  const [currentSlide, setCurrentSlide] = React.useState(0);
-  const [pause, setPause] = React.useState(false);
-  const timer = React.useRef();
   const datas = [
     {
-      id: "1",
-      img: require("../../../assets/images/cube.png"),
-      title: "Cube Charcoal Briquette",
+      id: '1',
+      img: require('../../../assets/images/cube.png'),
+      title: 'Cube Charcoal Briquette',
       content: [
-        "27x27x27 mm (56 pcs per 1 kg)",
-        "20x20x20 mm (120 pcs per 1 kg).",
-        "26x26x26 mm (64 pcs per 1 kg).",
-        "25x25x25 mm (72 pcs per 1 kg).",
-        "22x22x22 mm (96 pcs per 1 kg).",
+        '27x27x27 mm (56 pcs per 1 kg)',
+        '20x20x20 mm (120 pcs per 1 kg).',
+        '26x26x26 mm (64 pcs per 1 kg).',
+        '25x25x25 mm (72 pcs per 1 kg).',
+        '22x22x22 mm (96 pcs per 1 kg).',
       ],
     },
     {
-      id: "2",
-      img: require("../../../assets/images/hexagonal.png"),
-      title: "Hexagonal Charcoal Briquette",
-      content: ["20x35 mm (94 pcs per 1 kg).", "20x20 mm (64 pcs per 1 kg)."],
+      id: '2',
+      img: require('../../../assets/images/hexagonal.png'),
+      title: 'Hexagonal Charcoal Briquette',
+      content: ['20x35 mm (94 pcs per 1 kg).', '20x20 mm (64 pcs per 1 kg).'],
     },
     {
-      id: "3",
-      img: require("../../../assets/images/pillow.png"),
-      title: "Pillow Charcoal Briquette",
-      content: ["50x50x25 mm (50 pcs per 1 kg)"],
+      id: '3',
+      img: require('../../../assets/images/pillow.png'),
+      title: 'Pillow Charcoal Briquette',
+      content: ['50x50x25 mm (50 pcs per 1 kg)'],
     },
     {
-      id: "4",
-      img: require("../../../assets/images/cylinder.png"),
-      title: "Cylinder Charcoal Briquette",
+      id: '4',
+      img: require('../../../assets/images/cylinder.png'),
+      title: 'Cylinder Charcoal Briquette',
       content: [
-        "18x50 mm (72 pcs per 1 kg).",
-        "18x35 mm (102 pcs per 1 kg).",
-        "20x50 mm (64 pcs per 1 kg).",
-        "20x35 mm (94 pcs per 1 kg).",
+        '18x50 mm (72 pcs per 1 kg).',
+        '18x35 mm (102 pcs per 1 kg).',
+        '20x50 mm (64 pcs per 1 kg).',
+        '20x35 mm (94 pcs per 1 kg).',
       ],
     },
-  ];
-
-  const [sliderRef, slider] = useKeenSlider({
-    initial: 1,
-    slideChanged(s) {
-      setCurrentSlide(s.details().relativeSlide);
-    },
-    loop: true,
-    duration: 5000,
-    dragStart: () => {
-      setPause(true);
-    },
-    dragEnd: () => {
-      setPause(false);
-    },
-  });
-
-  React.useEffect(() => {
-    timer.current = setInterval(() => {
-      if (!pause && slider) {
-        slider.next();
-      }
-    }, 5000);
-    return () => {
-      clearInterval(timer.current);
-    };
-  }, [pause, slider]);
+  ]
 
   return (
     <div className="section-wrap">
       <h1 className="section-title">Type Of Products</h1>
       <div className="slider-wrap">
-        <div ref={sliderRef} className="keen-slider">
+        <Slider>
           {datas.map((data) => (
             <Row key={data.id} className="keen-slider__slide number-slide1">
               <Col span={15}>
@@ -90,24 +61,9 @@ const ProductSection = () => {
               </Col>
             </Row>
           ))}
-        </div>
-        {slider && (
-          <div className="dots">
-            {[...Array(slider.details().size).keys()].map((idx) => {
-              return (
-                <button
-                  key={idx}
-                  onClick={() => {
-                    slider.moveToSlideRelative(idx);
-                  }}
-                  className={`dot ${currentSlide === idx ? "active" : ""}`}
-                />
-              );
-            })}
-          </div>
-        )}
+        </Slider>
       </div>
     </div>
-  );
-};
-export default ProductSection;
+  )
+}
+export default ProductSection
