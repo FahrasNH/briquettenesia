013ddad8773972c4bbd918cea36b9faf78002d67
FahrasNH <fahras1998@yahoo.com>
2021-12-18 13:36:59 +0700

    Adding replacer

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withImage = require('next-images')
 const withSass = require('@zeit/next-sass')
 const withCss = require('@zeit/next-css')
 
+module.exports = {
+  env: {
+    PUBLIC_URL: "https://fahrasnh.github.io/briquettenesia",
+    assetPrefix: './'
+  }
+
+}
+
 module.exports = withPlugins(
   [
     withSass,
