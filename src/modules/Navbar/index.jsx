1df58df527cb616b82d41b0d3b5b3fd325ac403d
imamnghr <imamn208@gmail.com>
2021-12-25 18:50:24 +0700

    Responsive navbar

--- src/modules/Navbar/index.jsx
+++ src/modules/Navbar/index.jsx
@@ -1,85 +1,104 @@
-import { Col, Row } from 'antd'
-import React from 'react'
-import { Link } from 'react-scroll'
+import { Col, Row } from "antd";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
 
 const Navbar = ({ children, className }) => {
   const navbarItems = [
     {
-      label: 'Home',
-      target: 'home',
+      label: "Home",
+      target: "home",
     },
     {
-      label: 'About Us',
-      target: 'about',
+      label: "About Us",
+      target: "about",
     },
     {
-      label: 'Products',
-      target: 'product',
+      label: "Products",
+      target: "product",
     },
     {
-      label: 'Contact',
-      target: 'contact',
+      label: "Contact",
+      target: "contact",
     },
-  ]
+  ];
 
+  const [isOpen, setOpen] = useState(false);
+
+  function openMenu() {
+    setOpen(!isOpen);
+  }
   return (
     <div className={className}>
       <Row className="navbar-wrap">
-        <Col>
+        <Col xs={24} sm={24} md={24} lg={6}>
           <h1>Briquettenesia</h1>
         </Col>
-        <Col>
-          <ul>
-            <li>
-              <Link
-                to="principle"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-              >
-                Principles
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-              >
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="product"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-              >
-                Products
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+        <Col xs={24} sm={24} md={24} lg={6}>
+          <div className="menu">
+            <ul>
+              <li>
+                <Link
+                  to="principle"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                >
+                  Principles
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="product"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                >
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
         </Col>
       </Row>
+      <div className="burger" onClick={openMenu}>
+        <div
+          className={`x ${isOpen ? "collapse rotate30 rotate45" : ""}`}
+        ></div>
+        <div
+          className={`y ${isOpen ? "collapse" : ""}`}
+          style={{ display: isOpen ? "none" : "block" }}
+        ></div>
+        <div
+          className={`z ${isOpen ? "collapse rotate125 rotate140" : ""}`}
+        ></div>
+      </div>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
